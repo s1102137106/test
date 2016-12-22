d4b87507ca754dc29310b4f703d164944adeb738
joe <s56565566123@gmail.com>
2016-12-22 15:25:25 +0800

    a

---
+++ ConsoleApplication1/ConsoleApplication1/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Class1
+    {
+        public static int a = 0;
+
+        public int get(){
+            return a;
+        }
+    }
+}
